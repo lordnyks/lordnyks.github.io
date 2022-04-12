c1042b5660a4f9c4723650cc759f0d5d84652b50
lordnyks <lordnyks@gmail.com>
2022-04-12 18:19:58 +0200

    commit

---
+++ index.js
@@ -0,0 +1,22 @@
+var seener = new Vue({
+    el: '#app',
+    data: {
+      seen: true
+    }
+  })
+  
+var clicker = new Vue({
+    el: '#clicker',
+    data: {
+        name: 'Vue.js'
+    },
+    methods:
+    {
+        test: function () {
+            
+            seener.seen == true ? seener.seen = false : seener.seen = true;
+        }
+    }
+
+})
+//   seener.seen = true;
